3c0a337ae2abff3380f84ed01b74105da6752b57
king <50609288+Aaron-23@users.noreply.github.com>
2019-11-08 10:32:17 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,5 @@
 # do something before mysql start
 ## read env and create mysql config file
 /usr/local/bin/env2file create --format mysql --path /etc/mysql/conf.d/custom.cnf
-/usr/local/bin/mysqld_exporter &
 # start mysql
 exec /docker-entrypoint.sh "$@"
